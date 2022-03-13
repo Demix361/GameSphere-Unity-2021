5782c4dbb881ae969d45945dd492c81e1e6c8b7b
Demix361 <49446810+Demix361@users.noreply.github.com>
2022-03-14 01:45:10 +0300

    Обновлены задние фоны

--- Homework1/Assets/Scripts/GameMechanics/Backgrounds/Background3.cs
+++ Homework1/Assets/Scripts/GameMechanics/Backgrounds/Background3.cs
@@ -21,19 +21,6 @@ namespace GameMechanics
                 var pos = new Vector2(Random.Range(-width, width), Random.Range(-height, height));
                 Instantiate(starPrefab, pos, Quaternion.identity, transform);
             }
-
-            //StartCoroutine(SpawnStars());
-        }
-
-        private IEnumerator SpawnStars()
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(Random.Range(0.01f, 0.1f));
-
-                var pos = new Vector2(Random.Range(-width * 0.1f, width * 0.1f), Random.Range(-height * 0.1f, height * 0.1f));
-                Instantiate(starPrefab, pos, Quaternion.identity, transform);
-            }
         }
     }
 }
---
+++ Homework1/Assets/Scripts/GameMechanics/Backgrounds/Background5.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GameMechanics
+{
+    public class Background5 : MonoBehaviour
+    {
+        [SerializeField] private GameObject starPrefab;
+        [SerializeField] private Transform planetTransform;
+        private int starAmount = 200;
+        private float height;
+        private float width;
+        
+        private void Start()
+        {
+            height = Camera.main.orthographicSize;
+            width = height * Camera.main.aspect;
+
+            for (int i = 0; i < starAmount; i++)
+            {
+                var pos = new Vector2(Random.Range(-width, width), Random.Range(-width, width));
+                Instantiate(starPrefab, pos, Quaternion.identity, transform);
+            }
+            
+            StartCoroutine(SpawnStars());
+        }
+
+        private void Update()
+        {
+            planetTransform.Rotate(0f, 0f, 1f * Time.deltaTime);
+        }
+        
+        private IEnumerator SpawnStars()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+
+                var pos = new Vector2(width * 1.1f, Random.Range(-height, height));
+                Instantiate(starPrefab, pos, Quaternion.identity, transform);
+            }
+        }
+    }
+}
